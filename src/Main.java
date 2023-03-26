459f24bee62070cc2c8dd8ec11386193f2ac9af2
ljs <3096471433@qq.com>
2023-03-26 14:39:30 +0800

    commit 3

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,8 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Hello world!");
         System.out.println("Hello world1!");
+        dog s = new dog();
+        System.out.println(s.a);
+
     }
 }
---
+++ src/dog.java
@@ -0,0 +1,3 @@
+public class dog {
+    int a=9;
+}
